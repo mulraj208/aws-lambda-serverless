73bdc329cd33efc78a36ad31e4172fe4e7d8817a
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-08-14 10:18:20 +0530

    Initial commit

---
+++ build/config/dw.js
@@ -0,0 +1,15 @@
+module.exports = {
+    SHORT_CODE: "kv7kzm78",
+    ORGANIZATION_ID: "f_ecom_zzuz_008",
+    SITE_ID: "RefArch",
+    CLIENT_ID: "6e3efb13-ae86-4c10-bc40-6dd3df58b56d",
+    TENANT_ID: "zzuz-008",
+    EINSTEIN_SITE_ID: "aaij-MobileFirst",
+    EINSTEIN_HOST: "https://api.cquotient.com",
+    EINSTEIN_ID: "1ea06c6e-c936-4324-bcf0-fada93f83bb1",
+    EINSTEIN_IS_PRODUCTION: false,
+    PROJECT_ID: "pwa-kit-demo-poc",
+    SANDBOX_URL: "https://zzuz-008.dx.commercecloud.salesforce.com/",
+    API_COMMERCE_CLOUD: "kv7kzm78.api.commercecloud.salesforce.com",
+    DX_COMMERCE_CLOUD: "zzuz-008.dx.commercecloud.salesforce.com"
+}

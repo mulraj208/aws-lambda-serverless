436993ce4ac5c91cca6f6c55acb06a93490b0adf
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-08-20 19:01:36 +0530

    Update static asset handler

--- handle-static-asset/handler.js
+++ handle-static-asset/handler.js
@@ -1,4 +1,5 @@
 const AWS = require('aws-sdk');
+const mime = require('mime-types');
 const url = require('url');
 
 const BUCKET_NAME = 'api-gateway-s3-pwa-kit'
@@ -10,19 +11,20 @@ exports.handler = async (event) => {
         const s3Key = path.substring(1); // Remove leading slash
 
         await s3.headObject({ Bucket: BUCKET_NAME, Key: s3Key }).promise();
-        const objectUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${s3Key}`;
+        const file = await s3.getObject({ Bucket: BUCKET_NAME, Key: s3Key }).promise();
+        const contentType = mime.lookup(s3Key) || 'application/octet-stream'; // Determine MIME type
 
         return {
-            statusCode: 302,
-            headers: {
-                Location: objectUrl
-            }
+            statusCode: 200,
+            headers: { 'Content-Type': contentType },
+            body: file.Body.toString('utf-8'),
         };
     } catch (err) {
-        console.error(err);
+        const path = url.parse(event.path).pathname;
+
         return {
             statusCode: 404,
-            body: JSON.stringify({ message: 'File not found' })
+            body: JSON.stringify({ message: `File not found. path: ${path}` })
         };
     }
 };
